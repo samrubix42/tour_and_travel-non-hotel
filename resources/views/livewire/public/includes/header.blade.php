5b46399bb89e5c47940b42d84adb2c1414c25a20
Saumy Anand <samcool3203@gmail.com>
2025-12-04 15:30:02 +0530

    header updated

--- resources/views/livewire/public/includes/header.blade.php
+++ resources/views/livewire/public/includes/header.blade.php
@@ -2,11 +2,11 @@
        <!-- start navigation -->
        <nav class="navbar navbar-expand-lg bg-transparent  border-radius-6px md-border-radius-0px">
            <div class="container-fluid">
-               <div class="col-auto col-lg-1 me-lg-0 me-auto">
+               <div class="col-auto col-lg-2 me-lg-0 me-auto">
                    <a class="navbar-brand" href="{{ route('home') }}">
-                       <p class="teerth-title" style="color: white; font-size: 22px; font-weight: bold; margin: 10px;">
-                           TeerthYatraHoliday
-                       </p>
+                       <img src="{{ asset('asset/image/1000400474-removebg-preview.png') }}"
+                           alt="Logo"
+                           style="width:160px; height:auto; object-fit:contain;">
 
                        <style>
                            @media (max-width: 768px) {
@@ -27,31 +27,33 @@
                    </button>
                    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
                        <ul class="navbar-nav navbar-left justify-content-end" style="display:flex; flex-wrap:nowrap; white-space:nowrap;">
-
-                           {{-- <li class="nav-item dropdown submenu">
+                           <li class="nav-item">
+                               <a href="{{ route('home') }}" class="nav-link">Home</a>
+                           </li>
+                           <li class="nav-item dropdown submenu">
                                <a href="{{ route('destination') }}" class="nav-link">Destination</a>
-                           <i class="fa-solid fa-angle-down dropdown-toggle" id="navbarDropdownMenuLink1" role="button" data-bs-toggle="dropdown" aria-expanded="false"></i>
-                           <div class="dropdown-menu submenu-content" aria-labelledby="navbarDropdownMenuLink1">
-                               <div class="d-lg-flex mega-menu m-auto flex-column">
-                                   <div class="row row-cols-1 row-cols-lg-4 row-cols-md-3 row-cols-sm-2 mb-40px md-mb-25px xs-mb-15px">
-                                       @foreach($destinations->chunk(8) as $chunk)
-                                       @foreach($chunk as $dest)
-                                       <div class="col">
-                                           <a href="{{ route('tour') }}?slug={{ $dest->slug }}" class="text-decoration-none text-dark d-block py-2">
-                                               <div class="d-flex align-items-center justify-content-between">
-                                                   <div>
-                                                       <strong class="fs-14" style="font-size:14px;">{{ $dest->name }}</strong>
+                               <i class="fa-solid fa-angle-down dropdown-toggle" id="navbarDropdownMenuLink1" role="button" data-bs-toggle="dropdown" aria-expanded="false"></i>
+                               <div class="dropdown-menu submenu-content" aria-labelledby="navbarDropdownMenuLink1">
+                                   <div class="d-lg-flex mega-menu m-auto flex-column">
+                                       <div class="row row-cols-1 row-cols-lg-4 row-cols-md-3 row-cols-sm-2 mb-40px md-mb-25px xs-mb-15px">
+                                           @foreach($destinations->chunk(8) as $chunk)
+                                           @foreach($chunk as $dest)
+                                           <div class="col">
+                                               <a href="{{ route('tour') }}?slug={{ $dest->slug }}" class="text-decoration-none text-dark d-block py-2">
+                                                   <div class="d-flex align-items-center justify-content-between">
+                                                       <div>
+                                                           <strong class="fs-14" style="font-size:14px;">{{ $dest->name }}</strong>
+                                                       </div>
+                                                       <i class="fa-solid fa-angle-right text-muted small"></i>
                                                    </div>
-                                                   <i class="fa-solid fa-angle-right text-muted small"></i>
-                                               </div>
-                                           </a>
+                                               </a>
+                                           </div>
+                                           @endforeach
+                                           @endforeach
                                        </div>
-                                       @endforeach
-                                       @endforeach
-                                   </div>
 
+                                   </div>
                                </div>
-                           </div>
                            </li>
                            <li class="nav-item dropdown submenu">
                                <a href="{{ route('experience') }}" class="nav-link">Experiences</a>
@@ -77,55 +79,17 @@
                                        </div>
                                    </div>
                                </div>
-                           </li>--}}
-                           <li class="nav-item">
-                               <a href="{{ route('home') }}" class="nav-link">Home</a>
                            </li>
-                           <li class="nav-item">
-                               <a href="{{ route('about') }}" class="nav-link">Teerth Yatra</a>
-                           </li>
-                           <li class="nav-item dropdown submenu">
-                               <a href="{{ route('destination') }}" class="nav-link">Tours In India</a>
-                               <i class="fa-solid fa-angle-down dropdown-toggle" id="navbarDropdownMenuLink1" role="button" data-bs-toggle="dropdown" aria-expanded="false"></i>
-                               <div class="dropdown-menu submenu-content" aria-labelledby="navbarDropdownMenuLink1">
-                                   <div class="d-lg-flex mega-menu m-auto flex-column">
-                                       <div class="row row-cols-1 row-cols-lg-4 row-cols-md-3 row-cols-sm-2 mb-40px md-mb-25px xs-mb-15px">
-                                           @foreach($destinations->chunk(8) as $chunk)
-                                           @foreach($chunk as $dest)
-                                           <div class="col">
-                                               <a href="{{ route('tour') }}?slug={{ $dest->slug }}" class="text-decoration-none text-dark d-block py-2">
-                                                   <div class="d-flex align-items-center justify-content-between">
-                                                       <div>
-                                                           <strong class="fs-14" style="font-size:14px;">{{ $dest->name }}</strong>
-                                                       </div>
-                                                       <i class="fa-solid fa-angle-right text-muted small"></i>
-                                                   </div>
-                                               </a>
-                                           </div>
-                                           @endforeach
-                                           @endforeach
-                                       </div>
 
-                                   </div>
-                               </div>
+
+                           <li class="nav-item">
+                               <a href="{{ route('blog') }}" class="nav-link">Blogs</a>
                            </li>
                            <li class="nav-item">
-                               <a href="{{ route('blog') }}" class="nav-link">International Tours</a>
+                               <a href="{{ route('about') }}" class="nav-link">About Us</a>
                            </li>
-                           <li class="nav-item">
-                               <a href="{{ route('hotels') }}" class="nav-link">Hotels</a>
-                           </li>
-                           <li class="nav-item">
-                               <a href="{{ route('contact') }}" class="nav-link">Taxi</a>
-                           </li>
-                           <li class="nav-item dropdown simple-dropdown">
-                               <a href="javascript:void(0);" class="nav-link">Pages</a>
-                               <i class="fa-solid fa-angle-down dropdown-toggle" id="navbarDropdownMenuLink3" role="button" data-bs-toggle="dropdown" aria-expanded="false"></i>
-                               <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink3">
-                                   <li><a href="{{ route('about') }}">About Us</a></li>
-                                   <li><a href="{{ route('blog') }}">Blog</a></li>
-                               </ul>
-                           </li>
+
+
 
                            <li class="nav-item">
                                <a href="{{ route('contact') }}" class="nav-link">Contact</a>
@@ -134,16 +98,16 @@
                        </ul>
                    </div>
                </div>
-               <div class="col-auto text-end col-lg-2">
+               <div class="col-auto text-end">
                    <div class="col-auto col-lg-2 text-end d-none d-sm-flex">
                        <div class="header-icon">
                            <div class="header-search-icon icon">
                                <a href="#" class="search-form-icon header-search-form"><i class="feather icon-feather-phone"></i></a>
-                              
+
                            </div>
-             
+
                        </div>
-                    
+
                    </div>
                </div>
            </div>
